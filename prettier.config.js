9c27985a9bb7b2b1bf8db8262e7b6ac69b056746
Serhii Horodilov <shorodilov@gmail.com>
2025-12-24 16:18:36 +0200

    feat: add Prettier with Tailwind CSS plugin
    Add Prettier and the Tailwind CSS plugin for consistent code formatting
    and optimized class ordering. Updated `package.json` with the new
    dependencies and included a `prettier.config.js` file for configuration.
    
    This enhances code readability and enforces best practices when using
    Tailwind CSS. Updated `yarn.lock` to reflect the new package installations.

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+// see: https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+    plugins: ["prettier-plugin-tailwindcss"],
+    semi: false,
+    singleQuote: false,
+}
