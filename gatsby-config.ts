0e635a33cb3a27f391e97cc42329f04ff05b7230
Serhii Horodilov <shorodilov@gmail.com>
2025-12-26 22:28:59 +0200

    fix: update siteUrl in gatsby-config for deployment

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,14 +3,14 @@ import type {GatsbyConfig} from "gatsby";
 const config: GatsbyConfig = {
     siteMetadata: {
         title: `CV Website`,
-        siteUrl: `https://www.yourdomain.tld`
+        siteUrl: `https://shorodilov.github.io`
     },
     // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
     // If you use VSCode you can also use the GraphQL plugin
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
     plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-        resolve: 'gatsby-plugin-manifest',
+        resolve: "gatsby-plugin-manifest",
         options: {
             "icon": "src/images/icon.png"
         }
