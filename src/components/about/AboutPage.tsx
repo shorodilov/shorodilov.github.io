d6a5527fdad4a798e600f1b09b9ae1e12d85a379
Serhii Horodilov <shorodilov@gmail.com>
2026-08-15 01:46:03 +0300

    fix: apply formatting

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -19,7 +19,7 @@ export const AboutPage = ({ children }: AboutPageProps) => (
 
     <main className="flex-1 px-page-gutter pt-2 pb-section md:pt-8">
       <MDXProvider components={aboutMdxComponents}>
-        <article className="mx-auto w-full max-w-readable type-body text-foreground [&>h2]:mt-8 [&>h2]:mb-3 [&>h2]:type-heading-2 [&>h3]:mt-6 [&>h3]:mb-2 [&>h3]:type-heading-4-caps [&>ol]:my-0 [&>ol]:list-decimal [&>ol]:pl-5 [&>p]:m-0 [&>p+p]:mt-2.5 [&>ul]:my-0 [&>ul]:list-disc [&>ul]:pl-5 [&_a]:text-accent [&_a]:underline [&_a]:underline-offset-4">
+        <article className="mx-auto w-full max-w-readable type-body text-foreground [&_a]:text-accent [&_a]:underline [&_a]:underline-offset-4 [&>h2]:mt-8 [&>h2]:mb-3 [&>h2]:type-heading-2 [&>h3]:mt-6 [&>h3]:mb-2 [&>h3]:type-heading-4-caps [&>ol]:my-0 [&>ol]:list-decimal [&>ol]:pl-5 [&>p]:m-0 [&>p+p]:mt-2.5 [&>ul]:my-0 [&>ul]:list-disc [&>ul]:pl-5">
           {children}
         </article>
       </MDXProvider>
